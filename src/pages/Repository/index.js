b86cc58ddc46743281bf11b8a837f0d3e43cf537
Lucas Procopio <lucas@eadskill.com.br>
2019-09-12 12:03:55 -0300

    feat: Add repository details

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import api from '../../services/api';
-import { Loading, Owner } from './styles';
+import { Loading, Owner, IssueList } from './styles';
 import Container from '../../Components/Container';
 
 class Repository extends React.Component {
@@ -52,6 +52,23 @@ class Repository extends React.Component {
           <h1>{repository.name}</h1>
           <p>{repository.description}</p>
         </Owner>
+
+        <IssueList>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map(label => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssueList>
       </Container>
     );
   }
